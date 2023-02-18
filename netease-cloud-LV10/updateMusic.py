14cbd1787c8f9aeaffcc02f3cd4910229a5e67cc
cwuom <74239856+cwuom@users.noreply.github.com>
2023-02-18 15:23:37 +0800

    Update updateMusic.py

--- netease-cloud-LV10/updateMusic.py
+++ netease-cloud-LV10/updateMusic.py
@@ -6,33 +6,58 @@ import json
 import random
 
 """
-仅供娱乐 效率可能不怎么样
+仅供娱乐 打压pdd上面拿这个卖钱的
 """
 
 
-sourceid = "1849703902" # 歌单ID
-play_time = "123" # 播放时间
-musicid = "22504151" # 歌曲ID
+succeed = 0
+failed = 0
 
 def main():
-    global x
-    cookies={}
+    global succeed
+    global failed
+    cookies = {}
     f = open("cookie.txt", "r")
     for line in f.read().split(';'):
-        name,value=line.strip().split('=',1)
-        cookies[name]=value  #为字典cookies添加内容
+        name, value = line.strip().split('=', 1)
+        cookies[name] = value
 
-    headers = {'content-type': "application/json", "User-Agent" : "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70"}
+    headers = {'content-type': "application/json",
+               "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/109.0.0.0 Safari/537.36 Edg/109.0.1518.70"}
 
     while True:
+        t1 = Thread(target=play, args=(headers, cookies))
+        t1.start()
+        print("\rsucceed =", succeed, "| failed =", failed, end="", flush=True)
+
+
+def play(headers, cookies):
+    try:
+        global succeed
+        global failed
+        sourceid = "1849703902"  # 歌单ID
+        play_time = "123"  # 播放时间
+        musicid = "1381290206"  # 歌曲ID
+
         t = time.time()
         t = str(int(round(t * 1000)))
-        a = requests.get("http://127.0.0.1:3000/scrobble?id="+musicid+"&sourceid="+sourceid+"&time="+play_time+"&timestamp="+t, cookies=cookies, headers=headers)
-        print("刷了",x,"次")
-        x += 1
+        res = requests.get(
+            "http://127.0.0.1:3000/scrobble?id=" + musicid + "&sourceid=" + sourceid + "&time=" + play_time + "&timestamp=" + t,
+            cookies=cookies, headers=headers)
+
+        if res.text == """{"code":200,"data":"success","message":""}""":
+            succeed += 1
+        else:
+            failed += 1
+
+        t1 = Thread(target=play, args=(headers, cookies))
+        t1.start()
 
 
-for x in range(100): # 100个线程并发
-    t1 = Thread(target=main)
-    t1.start()
-    time.sleep(1)
+    except:
+        pass
+
+
+
+if __name__ == '__main__':
+    main()
